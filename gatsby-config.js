797fc5a9da19bcfcfe3f9ba3b3565ce2211780e0
Grant Glidewell <grant@thirdandgrove.com>
2019-05-17 08:52:49 -0700

    prepared for cloud instance

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,8 @@
-require("dotenv").config()
-
 module.exports = {
   siteMetadata: {
     title: `Gatsby Preview Test`,
     description: `Testing preview with drupal.`,
-    author: `Third and Grove`,
+    author: `Third and Grove`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -12,11 +10,9 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -26,17 +22,17 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-source-drupal`,
       options: {
-        baseUrl: process.env.DRUPAL_URL,
-      },
-    },
+        baseUrl: 'http://drupal.lgd.lndo.site'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
