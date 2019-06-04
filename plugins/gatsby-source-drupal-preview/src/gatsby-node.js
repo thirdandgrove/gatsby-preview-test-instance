1dfc409a6994ae047394ec1abe3f2d20617b7dd9
Grant Glidewell <grant@thirdandgrove.com>
2019-06-04 12:18:55 -0700

    added log for node data

--- plugins/gatsby-source-drupal-preview/src/gatsby-node.js
+++ plugins/gatsby-source-drupal-preview/src/gatsby-node.js
@@ -239,30 +239,29 @@ exports.sourceNodes = async (
   if (process.env.NODE_ENV === "development" && preview) {
     const server = micro(async (req, res) => {
       const request = await micro.json(req)
+
       const nodeToUpdate = JSON.parse(request).data
 
-      if (nodeToUpdate.id) {
-        const node = nodeFromData(nodeToUpdate, createNodeId)
-        // handle relationships ?? maybe ??
-        if (nodeToUpdate.relationships) {
-          _.each(nodeToUpdate.relationships, (v, k) => {
-            if (!v.data) return
-            if (_.isArray(v.data) && v.data.length > 0) {
-              // Create array of all ids that are in our index
-              node.relationships[`${k}___NODE`] = _.compact(
-                v.data.map(data =>
-                  ids[data.id] ? createNodeId(data.id) : null
-                )
-              )
-            } else if (ids[v.data.id]) {
-              node.relationships[`${k}___NODE`] = createNodeId(v.data.id)
-            }
-          })
-        }
-        node.internal.contentDigest = createContentDigest(node)
-        createNode(node)
-        console.log("\x1b[32m", `Updated node: ${node.id}`)
+      const node = nodeFromData(nodeToUpdate, createNodeId)
+      // handle relationships ?? maybe ??
+      if (nodeToUpdate.relationships) {
+        console.log(node)
+        _.each(nodeToUpdate.relationships, (v, k) => {
+          if (!v.data) return
+          if (_.isArray(v.data) && v.data.length > 0) {
+            // Create array of all ids that are in our index
+            node.relationships[`${k}___NODE`] = _.compact(
+              v.data.map(data => (ids[data.id] ? createNodeId(data.id) : null))
+            )
+          } else if (ids[v.data.id]) {
+            node.relationships[`${k}___NODE`] = createNodeId(v.data.id)
+          }
+        })
       }
+      node.internal.contentDigest = createContentDigest(node)
+      createNode(node)
+      console.log("\x1b[32m", `Updated node: ${node.id}`)
+
       res.end("ok")
     })
     server.listen(
